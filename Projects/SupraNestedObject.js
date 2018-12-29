8ab56061835f7b47bb01a5a0aca9bbe24cf9bd48
JCVogelsberg <intrepidjournalist@hotmail.com>
2018-12-28 17:02:11 -0800

    Finished Advanced Methods lessons - work computer

---
+++ Projects/SupraNestedObject.js
@@ -0,0 +1,28 @@
+let toyotaSupra = {
+
+  engine: {
+    model: '2JZ-GTE',
+    displacement: '3 liters',
+    turbocharged: true
+  },
+
+  transmission: {
+    type: 'automatic',
+    speeds: 4,
+    'stall speed': '4200 rpm'
+  },
+
+  suspension: {
+    struts: 'TEIN Flex',
+    swaybars: 'TRD',
+    EDFC: true,
+    'strut braces': 'carbon fiber'
+  },
+
+  wheels: {
+    rims: 'Work Meister S2R',
+    tires: 'Bridgestone Potenza S-03',
+    size: '17 inch'
+  }
+
+};

1da2ba173fa196241635e76f2454a66e99ee9b3e
JCVogelsberg <intrepidjournalist@hotmail.com>
2019-01-04 17:12:39 -0800

    Finished with Minesweeper project - work computer

---
+++ minesweeper/old-minesweeper.js
@@ -0,0 +1,8 @@
+const g = new Game(5,5,25);
+g.playMove(0,0);
+g.playMove(0,1);
+g.playMove(0,2);
+g.playMove(0,3);
+g.playMove(0,4);
+
+
--- minesweeper/src/board.js
+++ minesweeper/src/board.js
@@ -1,23 +1,4 @@
-class Game {
-  constructor(numberOfRows,numberOfColumns,numberOfBombs) {
-      this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
-  playMove(rowIndex,columnIndex) {
-    this._board.flipTile(rowIndex,columnIndex);
-    if ( this._board.playerBoard[rowIndex][columnIndex] === 'B' ) {
-      console.log('Game Over! Final Board:');
-      this._board.print();
-    } else if (!this._board.hasSafeTiles()) {
-      console.log('Congratulations, you won!');
-    } else {
-      console.log('Current board:');
-      this._board.print();
-    }
-  }
-}
-
-
-class Board {
+export default class Board {
   constructor(numberOfRows,numberOfColumns,numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
     this._numberOfTiles = (numberOfRows * numberOfColumns);
@@ -125,11 +106,3 @@ class Board {
   }
 };
 
-
-
-const g = new Game(5,5,25);
-g.playMove(0,0);
-g.playMove(0,1);
-g.playMove(0,2);
-g.playMove(0,3);
-g.playMove(0,4);

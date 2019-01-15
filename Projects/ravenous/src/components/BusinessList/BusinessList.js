045658499ce8fa27f4bf7f8519f24b880185418f
Jason Vogelsberg <intrepidjournalist@hotmail.com>
2019-01-14 20:49:25 -0800

    Finished first part of Ravenous React app - home computer

---
+++ Projects/ravenous/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,20 @@
+// Simulates what a returned list of businesses would look like in the app 
+
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
+};
+
+export default BusinessList;
